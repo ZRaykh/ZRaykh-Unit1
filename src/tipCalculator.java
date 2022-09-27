7e95230e4483bc2efa242b89cd02b9768399fb67
ZRaykh <zraykh2784@bths.edu>
2022-09-27 13:27:13 -0400

    Fixed decimal rounding Organized code

--- src/tipCalculator.java
+++ src/tipCalculator.java
@@ -1,31 +1,54 @@
 import java.util.Scanner;
+import java.text.DecimalFormat;
 public class tipCalculator {
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in); //setting up scanner
-        System.out.println("Welcome to tip calculator user!"); //Welcoming user to the program
 
-        System.out.print("First, please enter the amount of people paying with you:"); //Collecting initial inputs for people, tip percent, and initial bill
+        Scanner s = new Scanner(System.in); //setting up scanner
+        DecimalFormat df = new DecimalFormat("0.00"); // setting up decimal format
+
+        //Welcoming user to the program
+        System.out.println("Welcome to tip calculator user!");
+
+        //Collecting input for people paying
+        System.out.print("First, please enter the amount of people paying with you:");
         int people = s.nextInt();
+
+        //Collecting input for total bill without the tip
         System.out.print("Secondly, enter the total bill without the tip:");
         double initialBill = s.nextDouble();
-        System.out.print("Lastly, enter the tip percent you wish to tip, without the % sign:");
-        int tipPercent = s.nextInt(); //End of user inputs
 
-        double tipPercentRaw = tipPercent/100.0; //Total tip and bill with tip calculations
+        //Collecting input for tip %
+        System.out.print("Lastly, enter the tip percent you wish to tip, without the % sign:");
+        int tipPercent = s.nextInt();
+
+        //Converting tip percent to decimal format
+        double tipPercentRaw = tipPercent/100.0;
+
+        //Calculating the total dollar cost of the tip, then rounding
         double fullTip = initialBill * tipPercentRaw;
-        fullTip = Math.round(fullTip * 100);  //rounding off decimal points
-        fullTip /=100;
+        fullTip = Math.round(fullTip * 100);
+        fullTip /= 100;
+
+        //Calculating the total cost of the bill
         double fullBill = initialBill + fullTip;
 
-        double tipPerPerson = fullTip / people; //Tip and bill per person calculations
+        //Calculating the amount of the tip paid per person, then rounding
+        double tipPerPerson = fullTip / people;
         tipPerPerson = Math.round(tipPerPerson * 100);
-        tipPerPerson /= 100; //rounding off decimal points
-        double totalPerPerson = fullBill / people;
-        totalPerPerson = Math.round(totalPerPerson * 100); //rounding off decimal points
-        totalPerPerson /=100;
+        tipPerPerson /=100;
 
-        System.out.print("A bill with a tip of " + tipPercent + "% and an initial bill cost of $" + initialBill + " split among " + people + " people ");
-        System.out.println("will cost a total amount of $" + fullBill + " with a tip of $" + fullTip + ".");
-        System.out.println("This totals to $" + totalPerPerson + " per person, each paying $" + tipPerPerson + " of the tip.");
+        //Calculating the amount of the bill paid per person, tip included
+        double totalPerPerson = fullBill / people;
+        totalPerPerson = Math.round(totalPerPerson * 100);
+        totalPerPerson /= 100;
+
+        //Printing out the original inputs
+        System.out.print("A bill with a tip of " + tipPercent + "% and an initial bill cost of $" + df.format(initialBill) + " split among " + people + " people ");
+
+        //Printing the total costs of the tip and bill
+        System.out.println("will cost a total amount of $" + df.format(fullBill) + " with a tip of $" + df.format(fullTip) + ".");
+
+        //Printing what each person is paying
+        System.out.println("This totals to $" + df.format(totalPerPerson) + " per person, each paying $" + df.format(tipPerPerson) + " of the tip.");
     }
 }
